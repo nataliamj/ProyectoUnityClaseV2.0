ada338c63eb6c78f48c208997bd5ab3d58b6de2a
nataliamj <angynataliamejia@gmail.com>
2025-08-28 02:50:34 -0500

    Entrega parcial - Avance del proyecto Unity con personaje Pink Man

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -7,29 +7,32 @@ public class MainController : MonoBehaviour
     public float speed = 4f;
     public float jumpForce = 4f; // Aumentado significativamente
     private Rigidbody2D rb;
+    // Changed from Animation to Animator to use SetFloat
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        // Changed from Animation to Animator
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-       
         float move = Input.GetAxis("Horizontal");
         float jump = Input.GetAxis("Jump");
 
         if (jump != 0 && GroundChecking.isGround)
         {
-
             rb.velocity = new Vector2(0, speed);
         }
 
-        if (move!= 0)
+        if (move != 0)
         {
+            // Changed "Camina" to "Walk" for correct spelling and typical parameter naming
+            animator.SetFloat("Walk", move);
             rb.velocity = new Vector2(move * speed, rb.velocity.y);
         }
+        
     }
-
-  
 }
